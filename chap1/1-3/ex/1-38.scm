6bc3e8050680c9833c3493ad3fcd57b2dfed4b20
Norihiro Shimoda <norihiro.shimoda@gmail.com>
2012-02-05 20:24:25 +0900

    問題1.38を解いた

---
+++ chap1/1-3/ex/1-38.scm
@@ -0,0 +1,36 @@
+;; 問題1.37
+
+;; 再帰版のconf-fracを利用する
+(define (accumulate combiner null-value term a next b)
+  (if (> a b)
+      null-value
+      (combiner (term a)
+		(accumulate combiner null-value term (next a) next b))))
+
+(define (conf-frac n d k)
+  (define (conf-frac-combiner i bunbo)
+    (/ (n i) (+ (d i) bunbo)))
+  (define (conf-frac-term i) i)
+  (define (conf-frac-next i) (+ i 1))
+  (accumulate conf-frac-combiner (/ (n k) (d k)) conf-frac-term 1 conf-frac-next (- k 1)))
+
+
+(define (e-2 k)
+  (define (n i) 1.0)
+  (define (d i) 
+    (if (= (remainder i 3) 2)
+	(* 2 (+ 1 (quotient i 3)))
+	1))
+  (conf-frac n d k))
+
+(define (e k)
+  (+ 2 (e-2 k)))
+
+(e 7)
+;; gosh> 2.7183098591549295
+(e 8)
+;; gosh> 2.718279569892473
+(e 9)
+;; gosh> 2.718283582089552
+(e 10)
+;; gosh> 2.7182817182817183
