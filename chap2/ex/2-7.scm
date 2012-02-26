73db581297939147cb6ee5d503f9d3f3c499a9c9
Norihiro Shimoda <norihiro.shimoda@gmail.com>
2012-02-26 16:02:38 +0900

    問題2.7に使用例を追加

--- chap2/ex/2-7.scm
+++ chap2/ex/2-7.scm
@@ -24,4 +24,21 @@
 
 ;; 選択子upper-bound, lower-boundの実装
 (define (upper-bound x) (car x))
-(define (lower-bound x) (cdr y))
+(define (lower-bound x) (cdr x))
+
+
+;; 試してみる
+(define R1 (make-interval 7.48 6.12))
+(define R2 (make-interval 4.735 4.205))
+
+(upper-bound R1)
+;; gosh> 7.48
+(lower-bound R1)
+;; gosh> 6.12
+
+(add-interval R1 R2)
+;; gosh> (10.325 . 12.215)
+(mul-interval R1 R2)
+;; gosh> (25.7346 . 35.41780000000001)
+(div-interval R1 R2)
+;; gosh> (1.2925026399155226 . 1.7788347205707493)
