ca5388e0e00827164d04ef9346ec7596640abb42
Norihiro Shimoda <norihiro.shimoda@gmail.com>
2012-03-04 19:52:21 +0900

    問題2.15まで解いた

--- chap2/ex/2-12.scm
+++ chap2/ex/2-12.scm
@@ -1,5 +1,12 @@
 ;; 問題2.12
 
+;; 区間構成子
+(define (make-interval a b) (cons a b))
+
+;; 選択子upper-bound, lower-boundの実装
+(define (upper-bound x) (car x))
+(define (lower-bound x) (cdr x))
+
 ;; 中央値と許容誤差で表す数を扱う構成子と選択子
 (define (make-center-width c w)
   (make-interval (- c w) (+ c w)))
@@ -13,9 +20,9 @@
 
 ;; 中央値とパーセント許容誤差をとり、区間を返す構成子
 (define (make-center-percent c p)
-  (let ((percent (/ p 100.0)))
-    (make-interval (+ c (* c percent))
-		   (- c (* c percent)))))
+  (let ((per (/ p 100.0)))
+    (make-interval (- c (* c per))
+		   (+ c (* c per)))))
 
 (define (center i)
   (/ (+ (lower-bound i) (upper-bound i)) 2))
@@ -26,6 +33,7 @@
 
 ;; 10パーセントの許容誤差で6.8オーム(教科書P52)
 (define R1 (make-center-percent 6.8 10))
+R1
 ;; gosh> (7.4799999999999995 . 6.12)
 (center R1)
 ;; gosh> 6.8
---
+++ chap2/ex/2-14.scm
@@ -0,0 +1,44 @@
+;; 問題2.14
+
+(define (par1 r1 r2)
+  (div-interval (mul-interval r1 r2)
+		(add-interval r1 r2)))
+
+(define (par2 r1 r2)
+  (let ((one (make-interval 1 1 )))
+    (div-interval one
+		  (add-interval (div-interval one r1)
+				(div-interval one r2)))))
+
+;; par1とpar2で結果が異なることの確認
+(define R1 (make-center-percent 6.8 10))
+(define R2 (make-center-percent 4.7 5))
+(display R1)
+(display R2)
+(par1 R1 R2)
+;; gosh> (2.201031010873943 . 3.4873689182805854)
+(par2 R1 R2)
+;; gosh> (2.581558809636278 . 2.97332259363673)
+
+;; 幅が中央値に比べて小さいパーセントの区間を用いて確認
+(define R3 (make-center-percent 10 1))
+(display R3)
+(define R4 (make-center-percent 20 1))
+(display R4)
+
+(define div-R3 (div-interval R3 R3))
+(center div-R3)
+;; gosh> 1.0002000200020003
+(percent div-R3)
+;; gosh> 1.9998000199979908
+;; 誤差が1%から約2%へと増加している
+
+(define div-R3-R4 (div-interval R4 R3))
+(center div-R3-R4)
+;; gosh> 2.0004000400040005
+(percent div-R3-R4)
+;; gosh> 1.9998000199979908
+;; やはり誤差が増加している
+
+;; 区間同士の乗算及び除算を行うと誤差が
+;; 増加していくことが推察できる
