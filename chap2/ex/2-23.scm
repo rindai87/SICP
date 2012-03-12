6e2f9fdf15eaac3064b8396d0e49f14287816651
norihiro.shimoda <norihiro.shimoda@gmail.com>
2012-03-12 20:07:47 +0900

    問題2.23を追記

--- chap2/ex/2-23.scm
+++ chap2/ex/2-23.scm
@@ -14,3 +14,15 @@
 ;; 57
 ;; 321
 ;; 88#<undef>
+
+;; consで一つの式にしてしまい評価させてしまうという方法もある
+(define (for-eaach proc items)
+  (if (null? items)
+      #f
+      (cons (proc (car items)) (for-each proc (cdr items)))))
+
+;; beginを利用してもよい
+(define (for-eaach proc items)
+  (if (null? items)
+      #f
+      (begin (proc (car items)) (for-each proc (cdr items)))))
