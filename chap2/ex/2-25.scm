9da1c0cf000e152946f72aee8b257861172415bc
Norihiro Shimoda <norihiro.shimoda@gmail.com>
2012-03-18 00:24:10 +0900

    問題2.25を解いた

---
+++ chap2/ex/2-25.scm
@@ -0,0 +1,24 @@
+;; 問題2.25
+
+(1 3 (5 7) 9)
+(define x (list 1 3 (list 5 7) 9))
+(car (cdr (car (cdr (cdr x)))))
+;; gosh> 7
+;; cdr => cdr => car => cdr => car 
+
+((7))
+(define x (list (list 7)))
+(car (car x))
+;; gosh> 7
+;; car => car 
+
+(1 (2 (3 (4 (5 (6 7))))))
+(define x (list 1 (list 2 (list 3 (list 4 (list 5 (list 6 7)))))))
+(car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr x))))))))))))
+;; gosh> 7
+;; cdr => car
+;; => cdr => car 
+;; => cdr => car
+;; => cdr => car
+;; => cdr => car
+;; => cdr => car
