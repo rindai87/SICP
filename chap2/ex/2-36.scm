da10fd1d72660d59333538aba54b0b0869f5e5c2
Norihiro Shimoda <norihiro.shimoda@gmail.com>
2012-04-01 23:38:28 +0900

    ちょっと修正

---
+++ chap2/ex/2-36.scm
@@ -0,0 +1,10 @@
+;; 問題2.36
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      nil
+      (cons (accumulate op init (map car seqs))
+	    (accumulate-n op init (map cdr seqs)))))
+
+(accumulate-n + 0 (list (list 1 2 3) (list 4 5 6) (list 7 8 9) (list 10 11 12)))
+;; gosh> (22 26 30)
